a772fad72c77a2e8311b89d9ccb704b1b73177fc
Thanish-Maricherla <123317095+Thanish-Maricherla@users.noreply.github.com>
2023-01-22 21:51:59 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+no1 = 17
+no2 = 15
+
+sum = no1 + no2
+print('Sum of the two numbers is', sum )
+
+difference = no1 - no2
+print('Subtract of the two numbers is', difference )
+
+product = no1*no2
+print('Product of the two numbers is', product )
+
+division = no1%no2
+print('Remainder of the two numbers is', division )
